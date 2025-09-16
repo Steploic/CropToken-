c5cad707a6fd1e2d1aee95c6513c011fc0a4aa38
Steploic <112773471+Steploic@users.noreply.github.com>
2025-09-16 14:06:42 +0200

    Update ICropToken.sol

--- contracts/interfaces/ICropToken.sol
+++ contracts/interfaces/ICropToken.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface ICropToken is IERC20 {
+    function factory() external view returns (address);
+    function totalSupplyKg() external view returns (uint256);
+}
 
