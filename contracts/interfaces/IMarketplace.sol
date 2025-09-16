a6c93771a4d9c94dfbe5cfe66986fd1ae467158d
Steploic <112773471+Steploic@users.noreply.github.com>
2025-09-16 14:06:59 +0200

    Update IMarketplace.sol

--- contracts/interfaces/IMarketplace.sol
+++ contracts/interfaces/IMarketplace.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IMarketplace {
+    function createListing(uint256 cropId, uint256 pricePerToken, uint256 tokensToSell) external returns (uint256);
+    function buyTokens(uint256 listingId, uint256 quantity) external payable;
+    function cancelListing(uint256 listingId) external;
+}
 
