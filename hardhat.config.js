195f2095bed5a904d87eeaf7f262c7b6aee8e947
Steploic <112773471+Steploic@users.noreply.github.com>
2025-09-16 13:33:20 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,39 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
+const { HEDERA_ACCOUNT_ID, HEDERA_PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    "hedera-testnet": {
+      url: "https://testnet.hashio.io/api",
+      accounts: [HEDERA_PRIVATE_KEY],
+      chainId: 296,
+      gas: 3000000,
+      gasPrice: 100000000000 // 100 gwei
+    },
+    "hedera-mainnet": {
+      url: "https://mainnet.hashio.io/api", 
+      accounts: [HEDERA_PRIVATE_KEY],
+      chainId: 295,
+      gas: 3000000,
+      gasPrice: 100000000000
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
